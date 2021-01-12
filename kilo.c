3748f1d995356782dcfad2586c5139ce21ed8bdd
kalekotlin <kalekotlin@gmail.com>
2021-01-11 20:08:19 -0700

    Initial commit. Setting up attributes for the terminal. Start terminal in raw mode, then set it back to default.

---
+++ kilo.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+struct termios orig_termios;
+
+void disableRawMode() {
+    // Set the terminal attributes to the default, non-raw mode.
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawMode() {
+    tcgetattr(STDIN_FILENO, &orig_termios);
+
+    // Register this function to be called as the program exits.
+    atexit(disableRawMode);
+
+    // Termios struct holding flags. Set to the default struct.
+    struct termios raw = orig_termios;
+    
+    // Read attributes into the raw struct.
+    tcgetattr(STDIN_FILENO, &raw);
+
+    // "c_lflag" is used for local, or misc., flags.
+    // Use the NOT operater to flip the binary "...00001000" to "...11110111". 
+    // Use the AND operateor to flip the fourth bit in the flags field to become 0.
+    raw.c_lflag &= ~(ECHO);
+
+    // Applies the attributes in the struct to the terminal.
+    // "TCSAFLUSH" arg waits for pending output to be written to the terminal. 
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+int main() {
+    enableRawMode();
+
+    char c;
+    // Read 1 byte at a time from from the standard input into the variable c.
+    while (
+        read(STDIN_FILENO, &c, 1) == 1 
+        && c != 'q'
+    );
+
+    return 0;
+}
